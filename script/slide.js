5588263f2530be61203be548055a73d2958402f8
unknown <mkropidlowsky@gmail.com>
2022-04-21 07:42:31 +0200

    add project file

---
+++ script/slide.js
@@ -0,0 +1,16 @@
+
+if(document.body.offsetWidth <= 700){
+
+	new Splide( '#splide', {
+		type   : 'loop',
+		perPage: 1,
+		focus  : 'center',
+	}).mount();
+} else {
+	
+	new Splide( '#splide', {
+		type   : 'loop',
+		perPage: 1,
+		focus  : 'center',
+	}).mount();
+}
